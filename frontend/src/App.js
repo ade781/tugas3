db7ab78ca09d43b438ae3fcbe402b246983ac84e
ade781 <110089945+ade781@users.noreply.github.com>
2025-05-19 23:15:27 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,25 @@
+// App.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoteList from "./components/NoteList";
 import AddNote from "./components/AddNote";
 import EditNote from "./components/EditNote";
-import Login from "./components/Loginn";  // <-- perbaiki path import
+import Login from "./components/Loginn";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />           {/* ⬅️ Halaman login */}
-        <Route path="/note" element={<NoteList />} />
+        <Route path="/" element={<Login />} />
+
+
         <Route path="/add" element={<AddNote />} />
         <Route path="/edit/:id" element={<EditNote />} />
+
+        <Route element={<Layout />}>
+          <Route path="/note" element={<NoteList />} />
+
+        </Route>
       </Routes>
     </BrowserRouter>
   );
