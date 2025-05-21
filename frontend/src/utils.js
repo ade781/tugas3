adb77a5be74ca4f0bf3a605556308573b836a454
ade781 <110089945+ade781@users.noreply.github.com>
2025-05-21 19:34:05 +0700

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,2 +1,3 @@
- export const BASE_URL = "https://be-369642099634.us-central1.run.app";
+ export const BASE_URL = "https://be-499088901156.us-central1.run.app";
 // export const BASE_URL = "http://localhost:5000";
+
